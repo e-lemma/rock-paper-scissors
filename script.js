8eaedd9b869d7780329f22646afa4b8cd52a0d75
eyuale <157725293+e-lemma@users.noreply.github.com>
2025-01-27 15:29:08 +0000

    Add logic to get and return human choice, add player score variables

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // A "Rock Paper Scissors" game playable via the console
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   // Generate a random int between 1 and 3 inclusive
   const randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -11,5 +14,21 @@ function getComputerChoice() {
       return "paper";
     case 3:
       return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  const validChoices = ["rock", "paper", "scissors"];
+
+  let humanChoice = prompt(
+    "Enter your move! 'rock', 'paper' or 'scissors'"
+  ).toLowerCase();
+
+  const checkValidity = validChoices.includes(humanChoice);
+
+  if (checkValidity) {
+    return humanChoice;
+  } else {
+    return `${humanChoice} is not a valid choice! Please choose from ${validChoices}`;
   }
 }
